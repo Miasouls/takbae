b6e29ab816c3240594eb02c7c7aa6e88d28ca7e4
Miasouls <69750319+Miasouls@users.noreply.github.com>
2021-01-28 23:52:28 +0900

    Update test.py

--- test.py
+++ test.py
@@ -17,27 +17,7 @@ def get_message(update, context):
     chat_id = 1638812607
     text = update.message.text
     if '택배' in text:
-        webdriver_options = webdriver.ChromeOptions()
-        webdriver_options .add_argument('headless')
-        webdriver_options.add_argument('window-size=1920x1080')
-
-
-
-        chromedriver = 'chromedriver.exe'
-        driver = webdriver.Chrome(chromedriver, options=webdriver_options )
-
-        driver.get('https://www.cjlogistics.com/ko/tool/parcel/tracking')
-        driver.maximize_window()
-
-        elem = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/div[1]/input')
-        elem.click()
-        elem.send_keys('637529257326')
-        driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/div[2]/span[1]/input').click()
-        body = driver.find_element_by_css_selector('body')
-        body.send_keys(Keys.PAGE_DOWN)
-        driver.implicitly_wait(3)
-        driver.get_screenshot_as_file('택배.png')
-        driver.quit()
+        
         bot.send_photo(chat_id, open('택배.png','rb'))
 
 
